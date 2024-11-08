acc0c1e24831f87ce7ade862005f70b3d8b1d314
Awais Mehmood <awaisbinmehmoodahmed@gmail.com>
2024-11-09 01:12:33 +0500

    Update page.tsx

--- src/app/blog-3/page.tsx
+++ src/app/blog-3/page.tsx
@@ -38,7 +38,7 @@ const Blog = () => {
                 <p className="text-gray-600 mb-4">
                 The digital landscape is undergoing a profound shift with the rise of Web 3.0 and Generative AI, two transformative forces reshaping how we interact with the internet and technology. Web 3.0, with its decentralized structure, promises more control for users over their data, as opposed to the centralized model of Web 2.0. Powered by blockchain and decentralized networks, it aims to create a more open and user-centric web where privacy, security, and transparency are prioritized.
                 </p>
-                <blockquote className="bg-gray-100 p-4 italic border-l-4 border-green-500 text-gray-600 mb-4">
+                <blockquote className="bg-gray-100 p-4 italic border-l-4 border-color text-gray-600 mb-4">
                 As Web 3.0 grows, the integration of Generative AI will play a pivotal role in creating personalized, dynamic, and interactive content. AI can generate new ideas, automate content creation, and even design personalized digital experiences, bridging the gap between technology and human creativity in unprecedented ways.
                 </blockquote>
                 <p className="text-gray-600 mb-4">The combination of Web 3.0 and Generative AI is set to unlock new opportunities for innovation, collaboration, and digital ownership, paving the way for a more intelligent and decentralized internet. This era will redefine how we connect, create, and communicate online.</p>
